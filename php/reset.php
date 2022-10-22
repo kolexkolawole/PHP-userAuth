7e74af23a54af34c23f47236f7d39fb83da2de24
kolexkolawole <kolexkolawole@gmail.com>
2022-10-22 06:56:37 +0100

    commit

--- php/reset.php
+++ php/reset.php
@@ -1,16 +1,15 @@
 <?php
+session_start();
+if(isset($_POST['submit'])){
+    // $full_name = $_POST['full_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];//complete this;
+    // $newpassword = $_POST['newpassword']; //complete this;
 
+    resetPassword($email, $password);
+}
 
 function resetPassword($email, $password, $newpassword){
-    if(isset($_POST['submit'])){
-        // $full_name = $_POST['full_name'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];//complete this;
-        // $newpassword = $_POST['newpassword']; //complete this;
-    
-        resetPassword($email, $password);
-    }
-    
     $file_read = fopen("../storage/users.csv", "r");
    $file_write = fopen("../storage/temporary.csv", "w");
    while(($data = fgetcsv($file_read)) !== FALSE){
@@ -18,12 +17,12 @@ function resetPassword($email, $password, $newpassword){
         $data[2] = $password;
         echo '<script>
         alert(" Successful change password");
-        window.location.href="../forms/resetpassword_1.html";
+        window.location.href="../forms/resetpassword.html";
         </script>';
     }else{
         echo '<script>
         alert(" Sorry user does not exist");
-        window.location.href="../forms/resetpassword_1.html";
+        window.location.href="../forms/resetpassword.html";
         <script>';
     }
     fputcsv($file_write, $data);
